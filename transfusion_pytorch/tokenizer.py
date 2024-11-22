8ada3b57d595c52791b67ce2fd4f73d7879f6d5a
palashmr@ornl.gov <palashmr@ornl.gov>
2024-11-22 17:42:58 -0500

    Major: Matbertsci encode and decoder working
    encoode
    decode
    check encoding by decoding encoded file
    count number of unique tokens

--- transfusion_pytorch/tokenizer.py
+++ transfusion_pytorch/tokenizer.py
@@ -1,83 +1,145 @@
 import os
 import json
-from transformers import BertTokenizer
+from pathlib import Path
+import pickle
+import sys
+sys.path.append('..')
+import pandas as pd
+import numpy as np
+from argparse import ArgumentParser
 
-# Initialize the tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')  #matscibert
-#tokenizer = BertTokenizer.from_pretrained('matscibert')
+from normalize_text import normalize
 
-# Define the directory containing your text files
+import torch
+
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer
+)
+
 directory = '/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/cif2txt'
-
+#directory = '/lustre/orion/stf218/proj-shared/brave/transfusion-pytorch/temp'
 # Ensure the output directory exists
-output_directory = '/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/token_json'
+output_directory = '/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/token_json_matscibert'
 os.makedirs(output_directory, exist_ok=True)
 
+
+
+# if args.model_name == 'scibert':
+#     model_name = 'allenai/scibert_scivocab_uncased'
+#     to_normalize = False
+# elif args.model_name == 'matscibert':
+model_name = 'm3rg-iitd/matscibert'
+#     to_normalize = True
+# elif args.model_name == 'bert':
+#     model_name = 'bert-base-uncased'
+#     to_normalize = False
+# else:
+#     raise NotImplementedError
+
+model_revision = 'main'
+
+
+max_seq_length = 1024
+num_labels = 30000
+config_kwargs = {
+    'num_labels': num_labels,
+    'revision': 'main',
+    'use_auth_token': None,
+}
+config = AutoConfig.from_pretrained(model_name, **config_kwargs)
+
+tokenizer_kwargs = {
+    'use_fast': True,
+    'revision': model_revision,
+    'use_auth_token': None,
+    'model_max_length': max_seq_length
+}
+tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
+
+
+def tokenize_function(examples):
+    examples = normalize(examples)
+    result = tokenizer(examples, padding=False, max_length=max_seq_length, truncation=True)
+    return result
+
 # Process each text file in the directory
-for filename in os.listdir(directory):
-    print(filename)
-    if filename.endswith(".txt"):
-        file_path = os.path.join(directory, filename)
-        output_file_path = os.path.join(output_directory, filename.replace('.txt', '.json'))
-        
-        # Read the text file
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-        
-        # Tokenize the text
-        tokenized = tokenizer(text, truncation=True, padding='max_length', max_length=2048, return_tensors="pt")
-        
-        # Convert tensor data to lists for JSON serialization
-        tokenized_data = {key: value.numpy().tolist() for key, value in tokenized.items()}
-        
-        # Save the tokenized data as JSON
-        with open(output_file_path, 'w', encoding='utf-8') as json_file:
-            json.dump(tokenized_data, json_file)
+def tokenize(directory):
+    for filename in os.listdir(directory):
+        print(filename)
+        if filename.endswith(".txt"):
+            file_path = os.path.join(directory, filename)
+            output_file_path = os.path.join(output_directory, filename.replace('.txt', '.json'))
+            
+            # Read the text file
+            with open(file_path, 'r', encoding='utf-8') as file:
+                text = file.read()
+            tokenized_data = tokenize_function(text)
+        #retxt = tokenizer.decode(tokenized_datasets['input_ids'])
+            #print(tokenized_data)
 
-print("Tokenization complete. Files saved to:", output_directory)
+            # Convert tensor data to lists for JSON serialization
+            tokenized_data = {key: value for key, value in tokenized_data.items()}
+            with open(output_file_path, 'w', encoding='utf-8') as json_file:
+                #print(tokenized_data)
+                json.dump(tokenized_data, json_file)
 
-# import os
-# import json
-# from transformers import BertTokenizer
-# import sys
+    print("Tokenization complete. Files saved to:", output_directory)
 
-# # Initialize the tokenizer
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')  # Or matscibert if specific
 
-# # Define the directory containing your text files
-# directory = '/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/cif2txt'
 
-# # Ensure the output directory exists
-# output_directory = '/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/token_text'
-# os.makedirs(output_directory, exist_ok=True)
+def decode(directory):
+     for filename in os.listdir(directory):
+        print(filename)
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as file:
+                token_data = json.load(file) ['input_ids']
+            print(token_data)
+            detokenized_data =   tokenizer.decode(token_data, skip_special_tokens=True)
+            print(detokenized_data)
 
-# # SLURM Variables
-# rank = int(os.getenv('SLURM_PROCID', 0))
-# size = int(os.getenv('SLURM_NTASKS', 1))
 
-# # List all files and split them
-# files = [f for f in os.listdir(directory) if f.endswith(".txt")]
-# # Distribute files to nodes
-# files = files[rank::size]
+import os
+import json
+from collections import Counter
 
-# # Process each text file assigned to this node
-# for filename in files:
-#     file_path = os.path.join(directory, filename)
-#     output_file_path = os.path.join(output_directory, filename.replace('.txt', '.json'))
-    
-#     # Read the text file
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         text = file.read()
-    
-#     # Tokenize the text
-#     tokenized = tokenizer(text, truncation=True, padding='max_length', max_length=1024, return_tensors="pt")
-    
-#     # Convert tensor data to lists for JSON serialization
-#     tokenized_data = {key: value.numpy().tolist() for key, value in tokenized.items()}
-    
-#     # Save the tokenized data as JSON
-#     with open(output_file_path, 'w', encoding='utf-8') as json_file:
-#         json.dump(tokenized_data, json_file)
+def count_tokens_in_directory(folder_path):
+    token_counter = Counter()
 
-# # Notify completion for this node
-# print(f"Tokenization complete for node {rank}. Files saved to:", output_directory)
+    # Iterate through all JSON files in the directory
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r') as file:
+                try:
+                    data = json.load(file)
+                    tokens = data.get("input_ids", [])
+                    token_counter.update(tokens)
+                except json.JSONDecodeError:
+                    print(f"Error reading file {filename}. Skipping...")
+
+    print("Number of unique tokens:", len(token_counter))
+    top_tokens_with_counts = token_counter.most_common(100)
+    top_tokens = [token for token, count in top_tokens_with_counts]
+
+    for token, count in top_tokens_with_counts:
+       
+        detokenized_data =   tokenizer.decode(token, skip_special_tokens=True)
+        print(f"Token {token}: {count} : {detokenized_data}")
+
+
+
+def main():
+    #tokenize(directory)
+
+    count_tokens_in_directory(output_directory)
+
+    # dec_directory = "/lustre/orion/stf218/proj-shared/brave/brave_database/junqi_diffraction/token_json_matscibert"
+    # decode(dec_directory)
+
+
+if __name__== '__main__':
+    main()
+
